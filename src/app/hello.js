d86f784156f7fb681f6fd81a206dc5e7a19f9e37
José Escorche <josesco5@gmail.com>
2017-05-13 15:08:04 -0400

    Add basic layout for view

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,6 +1,15 @@
 export const hello = {
   template: require('./hello.html'),
   controller() {
-    this.hello = 'Hello World!';
+    this.testCases = '';
+    this.result = '';
+
+    this.canSubmit = function () {
+      return this.testCases;
+    };
+
+    this.submit = function () {
+      this.result = this.testCases;
+    };
   }
 };
